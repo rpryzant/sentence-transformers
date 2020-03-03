7cc444f0eb56ba2ac82fa63249b1c921e11e1b54
rpryzant <rpryzant@stanford.edu>
2020-03-03 15:28:21 -0800

    sandbox ready to go

--- emb_sandbox/sandbox.py
+++ emb_sandbox/sandbox.py
@@ -3,9 +3,11 @@ import numpy as np
 from sklearn.metrics.pairwise import paired_cosine_distances, paired_euclidean_distances, paired_manhattan_distances
 from scipy.stats import pearsonr, spearmanr
 from sklearn.decomposition import TruncatedSVD
-
+import os
 from sklearn.cluster import KMeans, SpectralClustering
-
+from tqdm import tqdm
+import random
+import itertools
 
 def compute_pc(X,npc=1):
     """
@@ -35,12 +37,16 @@ def remove_pc(X, npc=1, pc=None):
     return XX
 
 
-def treat_clustering(emb1, emb2, npc, k):
-    # treating as sperate: 0.7252247783326363, npc=2 k=5
-    # concatting and then doing svd/clustering on the parts: 0.7364172032507696 (same args)
+def treat_clustering(emb1, emb2, npc, k, strategy='neither'):
+    # strategy = when to glue vectors: [neither, both, cluster]  
+    assert strategy in {'neither', 'both', 'cluster'}
 
     # cluster vectors
-    embs = np.concatenate((emb1, emb2), axis=1)
+    if strategy in {'both', 'cluster'}:
+        embs = np.concatenate((emb1, emb2), axis=1)
+    else:
+        embs = np.concatenate((emb1, emb2), axis=0)
+
     kmeans = KMeans(n_clusters=k).fit(embs)
 
     cluster_embs = [None] * k
@@ -50,39 +56,53 @@ def treat_clustering(emb1, emb2, npc, k):
         else:
             cluster_embs[cluster].append(emb)
 
-    # try spectral, kmeans
 
 
     # svd per cluster
     cluster_pcs = [None] * k
-    for cluster, embs in enumerate(cluster_embs):
-        cluster_pcs[cluster] = compute_pc(np.array(embs), npc)
+    for cluster, emb_group in enumerate(cluster_embs):
+        if strategy == 'cluster':
+            emb_group = list(np.array(emb_group)[:, :768]) + list(np.array(emb_group)[:, 768:])
 
-    #embs = np.concatenate((emb1, emb2), axis=0)
-    #pc = compute_pc(embs, NPC)
+        cluster_pcs[cluster] = compute_pc(np.array(emb_group), npc)
 
     # project each cluster + remove pcs
     # try matching clusters, 1 pc per cluster
     final_emb1 = []
     final_emb2 = []
     for e1, e2 in zip(emb1, emb2):
-        # e1c = kmeans.predict([e1])[0]
-        # e2c = kmeans.predict([e2])[0]
-        # e1 = remove_pc(e1, npc=npc, pc=cluster_pcs[e1c])
-        # e2 = remove_pc(e2, npc=npc, pc=cluster_pcs[e2c])
-        # final_emb1.append(e1)
-        # final_emb2.append(e2)
+        if strategy in {'cluster', 'both'}:
+            e1e2 = np.concatenate(([e1], [e2]), axis=1)
+            e1c = e2c = kmeans.predict(e1e2)[0]
+        else:
+            e1c = kmeans.predict([e1])[0]
+            e2c = kmeans.predict([e2])[0]
 
-        e1e2 = np.concatenate(([e1], [e2]), axis=1)
-        e1e2c = kmeans.predict(e1e2)[0]
-        e1e2 = np.squeeze(remove_pc(e1e2, npc=npc, pc=cluster_pcs[e1e2c]))
-        final_emb1.append(e1e2[:768])
-        final_emb2.append(e1e2[768:])
+
+        if strategy == 'both':
+            e1e2 = np.concatenate(([e1], [e2]), axis=1)
+            e1e2 = remove_pc(e1e2, npc=npc, pc=cluster_pcs[e1c])
+            e1 = e1e2[:, :768]
+            e2 = e1e2[:, 768:]
+        else:
+            # don't handle case (not glue_clustering and glue_svd) because 
+            # you have no way of grabbing the cluster assignments
+            e1 = remove_pc(e1, npc=npc, pc=cluster_pcs[e1c])
+            e2 = remove_pc(e2, npc=npc, pc=cluster_pcs[e2c])
+
+        final_emb1.append(e1)
+        final_emb2.append(e2)
 
     emb1 = np.squeeze(np.array(final_emb1))
     emb2 = np.squeeze(np.array(final_emb2))
 
     return emb1, emb2
+
+
+
+
+
+
 
 
 def treat_pc(emb1, emb2, npc):
@@ -93,23 +113,47 @@ def treat_pc(emb1, emb2, npc):
     return emb1, emb2
 
 
-input = sys.argv[1]
-NPC = 2
-K = 5
-
-f = np.load(open(input, 'rb'))
-emb1 = f['emb1']
-emb2 = f['emb2']
-labels = f['labels']
 
 
-# emb1, emb2 = treat_pc(emb1, emb2, npc=NPC)
-emb1, emb2 = treat_clustering(emb1, emb2, npc=NPC, k=K)
+
+# neither
+# 5 5 0.7351470132727655
+# both
+# 5 5 0.7333437322101058
+# cluster only
+# 5 5 0.7403271596663675
+
+replicates = sorted(
+    itertools.product(*[range(70), range(2, 20), ['neither', 'both', 'cluster']]),
+    key=lambda x: random.random())
+
+for NPC, K, strategy in replicates: 
+    NPC = 5
+    K = 5
+
+    correlations = []
+
+    # for f in tqdm(os.listdir('.')):
+    for f in os.listdir('.'):
+        if not f.endswith('.embs') or 'train' in f:
+            continue
+
+        f = np.load(open(f, 'rb'))
+        emb1 = f['emb1']
+        emb2 = f['emb2']
+        labels = f['labels']
 
 
-cosine_scores = 1 - paired_cosine_distances(emb1, emb2)
-corr = spearmanr(labels, cosine_scores)
+        # emb1, emb2 = treat_pc(emb1, emb2, npc=NPC)
+        emb1, emb2 = treat_clustering(emb1, emb2, npc=NPC, k=K,
+            strategy='cluster')
 
-print(corr)
+        cosine_scores = 1 - paired_cosine_distances(emb1, emb2)
+        corr = spearmanr(labels, cosine_scores).correlation
+        correlations.append(corr)
+
+    print(NPC, K, np.mean(correlations))
+    quit()
+
 
 
